1c51a0fc30b870c273ba51a63fc45c00436730ef
MAANYA UPADHYAYA <104818288+MaanyaUpadhyaya@users.noreply.github.com>
2023-10-22 14:55:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,41 +52,41 @@ mapping = dentalreport.allocate_indices(region_number)
 
 attributes = dentalreport.begin_end_mapping(attributes,mapping)
 
-confirm = input("\nContinue to generate a pre-filled report? (yes/no): ").lower()
+'''confirm = input("\nContinue to generate a pre-filled report? (yes/no): ").lower()
 if confirm == 'no' or confirm == 'n':
     print("No report will be generated. Thank you.")
-else:
-    windowed_pixel_array = imageprocess.get_windowed_pixels(ds)
+else:'''
+windowed_pixel_array = imageprocess.get_windowed_pixels(ds)
 
-    image = imageprocess.convert_pixel_to_image(windowed_pixel_array)
+image = imageprocess.convert_pixel_to_image(windowed_pixel_array)
 
-    image = imageannotate.annotate(ds, image)
+image = imageannotate.annotate(ds, image)
 
-    image.save("result.jpg")
+image.save("result.jpg")
         
-    images = imageprocess.find_panoramic_view_image(selected_folder)
+images = imageprocess.find_panoramic_view_image(selected_folder)
 
-    images.save("panaroma.jpg")
+images.save("panaroma.jpg")
 
-    images = imageprocess.find_panoramic_view_image(selected_folder)
+images = imageprocess.find_panoramic_view_image(selected_folder)
 
-    images.save("panaroma.jpg")
+images.save("panaroma.jpg")
 
-    template_file = 'report_template.docx' 
+template_file = 'report_template.docx' 
          
-    filename = dicomreader.get_patinet_name(ds)
-    report_filename = f"{filename}_{dentalreport.get_current_datetime()}.docx"
-    report_filepath = os.path.join(selected_folder, report_filename)
+filename = dicomreader.get_patinet_name(ds)
+report_filename = f"{filename}_{dentalreport.get_current_datetime()}.docx"
+report_filepath = os.path.join(selected_folder, report_filename)
         
-    template = DocxTemplate(template_file)
-    attributes = dentalreport.addvirtual_implant_save(attributes,num_of_implants,template)
+template = DocxTemplate(template_file)
+attributes = dentalreport.addvirtual_implant_save(attributes,num_of_implants,template)
         
-    dentalreport.render_save_report(template,attributes, report_filename)
+dentalreport.render_save_report(template,attributes, report_filename)
 
         
         
-    print("\nSuccessfully generated report!!")
-    print("\tAt:", selected_folder)
-    print("\tAs:", report_filename)
-    print()
+print("\nSuccessfully generated report!!")
+print("\tAt:", selected_folder)
+print("\tAs:", report_filename)
+print()
 
